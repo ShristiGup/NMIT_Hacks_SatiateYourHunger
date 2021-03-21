de9acbcca909fbad6337255d92df3638d855d963
ShristiGup <guptashristi25@gmail.com>
2021-03-21 07:42:47 +0530

    UI updations

--- sat_hunger/main_app/urls.py
+++ sat_hunger/main_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
+    path('', views.sati, name='sati'),
+    path('home/',views.home,name="home"),
 ]
--- sat_hunger/main_app/views.py
+++ sat_hunger/main_app/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def home(request):
     return render(request,'main_app/index.html')
+
+def sati(request):
+    return render(request, 'main_app/sati.html')
